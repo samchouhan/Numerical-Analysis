6700f8cf742a2235b6259101ac1827669755c370
samchouhan <sakshamchouhan8@gmail.com>
2026-09-06 21:39:44 +0530

    Fix comment formatting in Bisection method implementation

--- Bisection_Method_(From Scratch).py
+++ Bisection_Method_(From Scratch).py
@@ -1,5 +1,5 @@
 # To find the approximate value of the root of a polynomial using the Bisection method, implemented from 
-# scratch without using any pre-built modules.
+# Scratch without using any pre-built modules.
 
 def function(x):
     return x*x -4
