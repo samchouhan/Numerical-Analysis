2c35d639c715b9269f1df7a8d87d97874f1b5ed3
samchouhan <sakshamchouhan8@gmail.com>
2026-09-06 21:46:44 +0530

    Improve code formatting in Bisection method implementation

--- Bisection_Method_(From Scratch).py
+++ Bisection_Method_(From Scratch).py
@@ -1,14 +1,16 @@
-# To find the approximate value of the root of a polynomial using the Bisection method, implemented from 
+# To find the approximate value of the root of a polynomial using the Bisection method, implemented from
 # Scratch without using any pre-built modules.
 
-def function(x):
-    return x*x -4
 
-def bisection(a,b,tol=0.0001):
+def function(x):
+    return x * x - 4
+
+
+def bisection(a, b, tol=0.0001):
     if function(a) * function(b) >= 0:
         print("Bisection method fails.")
         return None
-    
+
     while (b - a) >= tol:
         c = (a + b) / 2
         if function(c) == 0:
@@ -18,6 +20,7 @@ def bisection(a,b,tol=0.0001):
         else:
             a = c
     return c
+
 
 root = bisection(1, 3)
 print(f"The root is approximately: {root:.4f}")
