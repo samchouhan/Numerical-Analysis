54e02a2e027edbfa9ce7d72bf3ecf4e170cf1d1e
samchouhan <sakshamchouhan8@gmail.com>
2026-09-07 21:49:22 +0530

    Add Newton-Raphson method implementation for root finding

---
+++ Newton-Raphson Method.py
@@ -0,0 +1,12 @@
+#To find the approximate value of the root of a polynomial using the Newton-Raphson method.
+from scipy.optimize import newton
+
+def function(x):
+    return x**2 - 4
+
+def df(x):
+    return 2*x
+
+# Find the root using the Newton-Raphson method
+root = newton(function, 1, fprime=df)
+print(f"The approximate value of the root is: {root:.4f}")
